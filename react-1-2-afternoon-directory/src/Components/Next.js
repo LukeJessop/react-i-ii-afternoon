649b878e98dbadfe66c74dc7ebd6d66a911006f2
unknown <jessop.lucas@gmail.com>
2020-10-22 14:05:56 -0600

    data restructure

---
+++ react-1-2-afternoon-directory/src/Components/Next.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class Next extends Component{
+    constructor(){
+        super()
+        this.state = {
+            
+        }
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Next
